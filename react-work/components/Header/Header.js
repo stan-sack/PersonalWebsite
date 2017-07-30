0db3b59c4dcbc0444d9f111b4bffc72035d9cfdf
Stan Tsouvallas <stsou@stsou-mbp.dolby.net>
2017-07-30 22:34:22 +1000

    add header and footer

---
+++ react-work/components/Header/Header.js
@@ -0,0 +1,59 @@
+import FontAwesome from 'react-fontawesome';
+import React from 'react';
+import s from './Header.css';
+
+class Header extends React.Component {
+
+    componentDidMount() {
+        window.componentHandler.upgradeElement(this.root);
+    }
+
+    componentWillUnmount() {
+        window.componentHandler.downgradeElements(this.root);
+    }
+
+    render() {
+        return (
+            <header className={s.header} >
+                <a href="au.linkedin.com/in/stantsouvallas">
+                    <FontAwesome
+                        name="linkedin-square"
+                        size="2x"
+                        inverse="true"
+                        aria-hidden="true"
+                        id={s.linkedinIcon}
+                    />
+                </a>
+                <a href="mailto:stan.tsouvallas@gmail.com">
+                    <FontAwesome
+                        name="envelope-square"
+                        size="2x"
+                        inverse="true"
+                        aria-hidden="true"
+                        id={s.emailIcon}
+                    />
+                </a>
+                <a href="au.linkedin.com/in/stantsouvallas">
+                    <FontAwesome
+                        name="facebook-square"
+                        size="2x"
+                        inverse="true"
+                        aria-hidden="true"
+                        id={s.facebookIcon}
+                    />
+                </a>
+                <a href="au.linkedin.com/in/stantsouvallas">
+                    <FontAwesome
+                        name="github-square"
+                        size="2x"
+                        inverse="true"
+                        aria-hidden="true"
+                        id={s.githubIcon}
+                    />
+                </a>
+            </header>
+        );
+    }
+}
+
+export default Header;
