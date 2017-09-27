f8b3fc6331ee54ab0c87d26ceda1f4acc6e47f21
Stan Tsouvallas <stsou@stsou-mbp.dolby.net>
2017-09-27 22:22:13 +1000

    sub in special css class for safari and ios

--- src/components/ImageSlide/ImageSlide.js
+++ src/components/ImageSlide/ImageSlide.js
@@ -2,13 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import s from './ImageSlide.css'
 import ProgressiveImage from 'react-progressive-image'
+import ClientJS from 'clientjs'
 
+const getClassName = () => {
+	let name = s.backgroundSlide
+	let client = new ClientJS()
+	let os = client.getOS().name
+	let browser = client.getBrowser().name
+	if (os === 'iOS' && browser === 'Safari') {
+		name = `${name} ${s.safari}`
+	}
+	return name
+}
 const ImageSlide = ({ backgroundImage, children, placeholder, id }) => (
 	<ProgressiveImage src={backgroundImage} placeholder={placeholder}>
 		{(src) => (
 			<div
 				id={id}
-				className={s.backgroundSlide}
+				className={getClassName()}
 				style={{ backgroundImage: `url(${backgroundImage})` }}>
 				{children}
 			</div>
